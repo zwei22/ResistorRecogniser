9137f9240a208485cf5296a1fa3b7d259ccc7c9b
zwei22 <wei851010@gmail.com>
2019-12-29 21:11:32 +0800

    haar cascade classifie

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
-import cv2
-import numpy as np
-
+from resReader import *
 from resFinder import *
+import numpy as np
+import cv2
+# import sys
+# sys.path.append('/usr/local/lib/python3.7/site-packages')
 
+CASCADE_PATH = './model/cascade.xml'
 
 def main():
 
-    cap = cv2.VideoCapture(1)
-    finder = resFinder()
+    cap = cv2.VideoCapture(0)
+    finder = resFinder(CASCADE_PATH)
+    reader = resReader()
 
-    while True:
+    while not cv2.waitKey(1) & 0xFF == ord('q'):
         ret, frame = cap.read()
         res = finder.find_res(frame)
-        # for r in res:
-        #     reader = resReader()
+
+        for (x, y, w, h) in res:
             
+            y, h = y+h//4, h//2
+            print(f'{x}, {y}, {w}, {h}')
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            reader.read_img(frame[x:x+w, y:y+h])
+            sorted_band = reader.read_band()
+            result = reader.read_value(sorted_band, frame)
+            cv2.putText(frame, result, (x, y-10), FONT, 1, (255,255,255),2,cv2.LINE_AA)
+
+        cv2.imshow('frame', frame)
+
 
     cap.release()
     cv2.destroyAllWindows()
--- resFinder.py
+++ resFinder.py
@@ -3,49 +3,15 @@ import numpy as np
 
 
 class resFinder:
-    def __init__(self):
+    def __init__(self, cascade_path):
         self.img = None
+        self.cascade_model = cv2.CascadeClassifier(cascade_path)
 
     def find_res(self, img):
-
-        # img = cv2.GaussianBlur(img, (7, 7), 0)
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        # hsv_lower = np.array([0,0,0])
-        # hsv_upper = np.array([40,80,255])
-        # mask = cv2.inRange(hsv, hsv_lower, hsv_upper)
-
-        # res = cv2.bitwise_and(img,img, mask= mask)
-        thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,31,10)
-        # thresh = cv2.erode(thresh,(3,3),iterations=7)
-        # thresh = cv2.dilate(thresh,(3,3),iterations=7)
-        # thresh = cv2.dilate(thresh,(3,3),iterations=7)
-        # thresh = cv2.erode(thresh,(3,3),iterations=7)
-
-        
-        # ret, thresh = cv2.threshold(gray, 60, 255, 0)
-        # thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,11,2)
-        # thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, (7,7))
-        # thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, (7,7))
-        cv2.imshow('th',thresh)
-        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        
-
-        for i, contour in enumerate(contours):
-            area = cv2.contourArea(contour)
-            if 100<area<10000:
-                cv2.drawContours(img, contours, i, (0,255,0), 3)
-                print(area, end='\t')
-        # print('')
-        
-
-
-        # image = np.hstack([img, cv2.bitwise_and(img,img, mask= mask)])
-        # cv2.imshow('frame', image)
-
+        resFind = self.cascade_model.detectMultiScale(gray, 1.1, 25)
+        return resFind
 
 
 if __name__ == '__main__':
-    pass
+    finder = resFinder('./cascade.xml')
